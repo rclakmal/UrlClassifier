d530965fe4303c4bed8bdbaf5272f6144b3b0316
Lakmal Padmakumara <lakmal.padmakumara@tum.de>
2016-01-16 15:53:25 +0100

    Updated the setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='urlclassifier',
+      version='0.1',
+      description='Provides functions to detect hidden statuses of web pages',
+      url='http://github.com/storborg/funniest',
+      author='Lakmal Padmakumara',
+      author_email='rclakmal@gmail.com',
+      license='MIT',
+      packages=['urlclassifier'],
+      zip_safe=False)
